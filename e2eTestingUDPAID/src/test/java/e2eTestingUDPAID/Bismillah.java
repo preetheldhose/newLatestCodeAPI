b533ad71b43779e23b899c3d42de4ec9e52c9593
Preeth Raj Eldhose <preeth.eldhose@preeth.net>
2022-06-16 17:05:17 -0700

    latest

---
+++ e2eTestingUDPAID/src/test/java/e2eTestingUDPAID/Bismillah.java
@@ -0,0 +1,11 @@
+package e2eTestingUDPAID;
+
+public class Bismillah {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bismillah");
+
+	}
+
+}
---
+++ e2eTestingUDPAID/src/test/java/reference/getAuthTokenCodeCopy.java
@@ -0,0 +1,137 @@
+package reference;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static io.restassured.RestAssured.given;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseBuilder;
+import io.restassured.filter.Filter;
+import io.restassured.filter.FilterContext;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.FilterableRequestSpecification;
+import io.restassured.specification.FilterableResponseSpecification;
+import io.restassured.specification.RequestSpecification;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class getAuthTokenCodeCopy {
+	
+	public static String respJWT_TokenID;
+	public static String final_respJWT_TokenID;
+	public static String store_TokenID_Value;
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Bismillah");
+		String store_TokenID_Value = getJWTToken();
+		System.out.println("This is the string value of TokenID : " + store_TokenID_Value);
+		getReferralOnBoardGetAdvCode();
+	}
+	
+	
+	final static String getJWTToken() throws JSONException {
+		String CONTEXT_PATH="https://login.microsoftonline.com/aa827ac4-4665-4dbb-98fa-fa4931a84709/oauth2/v2.0/token";
+		
+		Map<String, Object> bodyContent = new HashMap<String, Object>();
+		bodyContent.put("client_id", "07c0e1ac-9bcd-442f-b927-4b587ed346e9");
+		bodyContent.put("client_secret", "wsFJOp.wX8721f~c_5d3SXzMY-.3M3E2h~");
+		bodyContent.put("scope", "87963bba-d6c0-41af-ba93-ad00401e9f13/.default");
+		bodyContent.put("grant_type", "client_credentials");
+						  
+		Response response = given().
+		contentType("application/x-www-form-urlencoded").
+		formParams(bodyContent).
+		when().
+		post(CONTEXT_PATH).
+		then().
+		statusCode(200).
+		contentType("application/json;charset=utf-8").
+		extract().
+		response();
+		
+		JSONObject jsonObj = new JSONObject(response.prettyPrint());
+        String access_tk = jsonObj.getString("access_token");
+ 		
+		response.prettyPrint();
+		
+		String responseData = response.asString();
+		
+		int code=response.getStatusCode();
+		Assert.assertEquals(code, 200);
+		
+		respJWT_TokenID=responseData;
+		//System.out.println("getJWTToken respJWT_TokenID Data is as follows : " + respJWT_TokenID);
+		
+        String[] dataStr = respJWT_TokenID.split(":");
+        //System.out.println("getJWTToken dataStr : " + Arrays.toString(dataStr));
+        //System.out.println(("getJWTToken dataStr : " + dataStr[1]));
+        String[] dataFinal = dataStr[1].split(",");
+        //System.out.println("getJWTToken Final : " + Arrays.toString(dataFinal));
+        //System.out.println(("getJWTToken Final : " + dataFinal[0].toString()));
+        final_respJWT_TokenID=dataFinal[0];
+        //System.out.println("final_respJWT_TokenID : " + final_respJWT_TokenID);
+        final_respJWT_TokenID=access_tk;
+        //System.out.println("final_respJWT_TokenID : " + final_respJWT_TokenID);
+        return final_respJWT_TokenID;
+		
+	}
+	
+
+	final static void getReferralOnBoardGetAdvCode() throws JSONException {
+		//String CONTEXT_PATH="https://api.corp.frbnp2.com/lending/referral/v1/profile/fetch/code?code=travis58";
+		String CONTEXT_PATH="https://openfaas.udp1non.aws.nml.com/api/v1/service/domains/teenyverse/services/electricity_nightly";
+		String Token;
+		Token = final_respJWT_TokenID;
+						  
+		Response response = 
+				given()
+				  //.proxy("http://z_elnd_auto_proxy:Fj0zWBGJ%23F9%7D2ur61%25dn@vsproxynp2:8080")
+				  //.proxy("http://z_eaglelending_proxy:H8s8d%3D%23spL@vsproxynp2:8080 ")
+				  .relaxedHTTPSValidation()
+		          .headers("Authorization", "Bearer " + Token)	          
+		          .when()
+		          .get(CONTEXT_PATH)
+		          .then()
+		          .statusCode(200)
+		          .contentType("application/json")
+		          //.contentType("application/json; charset=utf-8")
+		          .extract()
+		          .response();
+		
+		JSONObject jsonObj = new JSONObject(response.prettyPrint());		
+		response.prettyPrint();
+		
+		String responseData = response.asString();
+		//System.out.println("Test1 getReferralOnBoardGetAdvCode Response Data is as follows : " + responseData);
+		
+		int code=response.getStatusCode();
+		//System.out.println("Test1 getReferralOnBoardGetAdvCode Status code is : " + code);
+		try {
+			Assert.assertEquals(code, 200);
+		} catch (Exception e) {
+			//System.out.println("Exception of Test1 getReferralOnBoardGetAdvCode : " + e);
+			
+		}		
+	
+	}
+	
+}
